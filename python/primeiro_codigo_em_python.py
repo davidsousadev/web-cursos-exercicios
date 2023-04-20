9ca5045b1cbe5b33e6b5aa6b81b4300231b07d1f
Davidplay <davidk1k3k@gmail.com>
2023-04-20 20:12:58 -0300

    primeiros passos

---
+++ python/primeiro_codigo_em_python.py
@@ -0,0 +1,17 @@
+n1 = float(input('Informe nota 1: '))
+n2 = float(input('Informe nota 2: '))
+m = (n1 + n2) / 2
+if m >= 6.0:
+ print('Aprovado:', m)
+else:
+ print('Reprovado:', m)
+
+
+n1 = 10
+n2 = 2.5
+nome = 'José'
+teste = True
+print(n1, n2, nome, teste)
+
+
+ 

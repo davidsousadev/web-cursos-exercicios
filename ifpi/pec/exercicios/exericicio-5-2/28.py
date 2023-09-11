7e5f1873f6e0bb385f254050cb49268d9d4f52e4
davidplay7 <72691307+davidplay7@users.noreply.github.com>
2023-09-10 21:58:02 -0300

    Update 28.py

--- ifpi/pec/exercicios/exericicio-5-2/28.py
+++ ifpi/pec/exercicios/exericicio-5-2/28.py
@@ -4,7 +4,7 @@
 def verificador(x):
     if (x == 'a'or x == 'e' or x == 'i' or x == 'o' or x == 'u' or x == 'A'or x == 'E' or x == 'I' or x == 'O' or x == 'U'):
         x = False
-    elif(s.isalpha()):
+    elif(x.isalpha()):
         x = True
     else:
         x = False

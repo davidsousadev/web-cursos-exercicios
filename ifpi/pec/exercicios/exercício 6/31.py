0a8b8e09bca1cc6831dc8a9bd95aa699618b99e2
davidk1k3k@gmail.com <davidk1k3k@gmail.com>
2023-09-12 15:34:45 -0300

    questões exercício 6

---
+++ ifpi/pec/exercicios/exercício 6/31.py
@@ -0,0 +1,6 @@
+def main():
+    s = str(input("Digite um nome:").strip())
+    print(f'Esse nome tem:',len(s),'caracteres.')
+    
+if __name__ == '__main__':
+    main()
---
+++ ifpi/pec/exercicios/exercício 6/32.py
@@ -0,0 +1,8 @@
+def main():
+    s = str(input("Digite um caractere: "))
+    cn = ord(s)
+    print(f'Esse e o código numérico {cn} correspondente a: {s}')
+
+if __name__ == '__main__':
+    main()
+    

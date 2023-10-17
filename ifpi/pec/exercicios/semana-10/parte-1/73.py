499a32c8f045397a6a2aa66ff3989614e389aada
Davidplay <davidk1k3k@gmail.com>
2023-10-17 17:21:51 -0300

    quesTão 3

--- ifpi/pec/exercicios/semana-10/parte-1/73.py
+++ ifpi/pec/exercicios/semana-10/parte-1/73.py
@@ -1,13 +1,17 @@
 '''
 Escreva um programa que leia um conjunto 100 números inteiros e exiba o valor médio dos mesmos (com duas casas decimais).
 '''
-def media():
+def media_de_numeros():
     media = 0
     for i in range(1,101):
         x = int(input())
         media += x
+    return media/100 
+
+
+   
 def main():
-    print(media/100)
+    print(media_de_numeros())
 
 
 if __name__ == '__main__':

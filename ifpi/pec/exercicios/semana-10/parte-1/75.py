0721f6f532bbe001dc1c5aa48dfab27ecf1b6edd
davidplay7 <davidk1k3k@gmail.com>
2023-10-18 08:15:23 -0500

    75 fail

--- ifpi/pec/exercicios/semana-10/parte-1/75.py
+++ ifpi/pec/exercicios/semana-10/parte-1/75.py
@@ -1,12 +1,19 @@
 '''
 Escreva um programa que leia um conjunto de 100 números inteiros positivos e determine o maior deles.
 '''
-maior = 0
-for i in range(1,101):
-    x = int(input())
-    if(x>=maior):
-        maior = x
-print(maior)
+
+def maior_numero():
+    maior = 0
+    for i in range(1,101):
+        x = int(input())
+        if(x>=maior):
+            maior = x
+    return maior
+
+
+
+def main():
+    print(maior_numero())
 
 
 

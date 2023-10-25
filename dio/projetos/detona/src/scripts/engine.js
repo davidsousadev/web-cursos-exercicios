be7c6fa51a55ded5a2c2e96905c79bd51ac0b308
davidplay7 <davidk1k3k@gmail.com>
2023-10-25 12:43:49 -0500

    Jogo finaliyzado

--- dio/projetos/detona/src/scripts/engine.js
+++ dio/projetos/detona/src/scripts/engine.js
@@ -6,9 +6,32 @@ const state = {
         score: document.querySelector("#score"),
     },
     values: {
-        timerId: null,
-        gameVelocity: 100,
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+    actions:{
+        timerId: setInterval(randomSquare,1000),
+        contDownTimerid: setInterval(counDown, 1000),
     }
+
+}
+
+function counDown(){
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+    if (state.values.currentTime <= 0){
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over: O seu resultado foi: " + state.values.result);
+    }
+}
+
+function playSound(audioname){
+    let audio = new Audio(`./src/audios/${audioname}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
 }
 
 function randomSquare(){
@@ -20,19 +43,27 @@ function randomSquare(){
     let randomNumber = Math.floor(Math.random()* 9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
 }
-function moveEnemy(){
-    
-    state.values.timerId = setInterval(randomSquare,state.values.gameVelocity);
-}
-function addListinerHitBox(){
+
+function addListenerHitBox(){
     state.view.squares.forEach((square) =>{
-        /*if(square.id === )*/
+        square.addEventListener("mousedown", ()=>{
+            
+            if(square.id === state.values.hitPosition){
+
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null; 
+                playSound("hit");       
+            }
+        });
+        
     });
 }
 
 function main(){
-    moveEnemy();
+    addListenerHitBox();
 }
 
 main();

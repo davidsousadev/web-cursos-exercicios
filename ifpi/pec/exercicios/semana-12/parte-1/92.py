528b3562e7eb858fbf462477d164e9d42db4d09e
davidplay7 <davidk1k3k@gmail.com>
2023-10-31 12:58:44 -0300

    inicio

--- ifpi/pec/exercicios/semana-12/parte-1/92.py
+++ ifpi/pec/exercicios/semana-12/parte-1/92.py
@@ -1,1 +1,10 @@
 '''Você tem uma poupança de 10 mil reais, que rende 0,7% ao mês. Você deseja comprar um carro, mas o preço do carro sobe a taxa de 0,4% ao mês. Escreva um programa que leia o preço de um carro hoje e calcule em quantos meses, com o dinheiro dessa aplicação, você terá dinheiro suficiente para comprar o carro à vista.'''
+
+l = 10000
+c = 0
+x = int(input())
+while x > l:
+    l = l + (l/100) * 0.7 
+    x = x + (x/100) * 0.4
+    c += 1 
+print(c)
--- ifpi/pec/exercicios/semana-12/parte-1/93.py
+++ ifpi/pec/exercicios/semana-12/parte-1/93.py
@@ -1,1 +1,13 @@
 '''Dado um país A, com taxa de natalidade de 2% ao ano, e um país B com uma taxa de natalidade de 3% ano. Sabe-se que, atualmente, o país A tem população maior que o país B. Faça um programa que leia a população de cada país e imprima o tempo necessário para que a população do país B ultrapasse a população do país A.'''
+
+l = int(input())
+c = 0
+x = int(input())
+while x > l:
+    if(l > 0 and x > 0):
+        l = l + (l/100) * 3 
+        x = x + (x/100) * 2
+        c += 1 
+    else:
+        break    
+print(c)

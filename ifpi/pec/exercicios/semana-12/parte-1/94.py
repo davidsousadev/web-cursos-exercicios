01e029ef5b7dbabfecba2e64042be377f6a327ab
davidplay7 <davidk1k3k@gmail.com>
2023-11-03 10:53:55 -0300

    questões 94/95

--- ifpi/pec/exercicios/semana-12/parte-1/94.py
+++ ifpi/pec/exercicios/semana-12/parte-1/94.py
@@ -1,1 +1,11 @@
 '''O número da sorte de uma pessoa é calculado somando os dígitos da sua data de nascimento. Escreva um programa que leia a data de nascimento, digitada no formado ddmmaaaa (um número inteiro com 8 dígitos), e mostre o seu número da sorte. Por exemplo, quem nasceu em 29/04/1989 deve digitar 29041989 e o programa vai calcular que o número da sorte é 42 (2 + 9 + 0 + 4 + 1 + 9 + 8 + 9 = 42).'''
+
+def calcular(data):
+    n = 0
+    while data > 0: 
+        u = data % 10
+        n += u
+        data = data // 10
+    return n
+
+print(f"{calcular(int(input()))}")
--- ifpi/pec/exercicios/semana-12/parte-1/95.py
+++ ifpi/pec/exercicios/semana-12/parte-1/95.py
@@ -1,3 +1,16 @@
 '''O dodô é uma ave não voadora, extinta atualmente, e que era endêmica da Ilha Maurítius, na costa leste da África. A partir do ano 1600, durante cada ano, 6% dos animais dos animais vivos no começo do ano morreram e o número de animais nascidos ao longo do ano que sobreviveram foi de 1% da população inicial.
 
 Escreva um programa que leia a população de aves no início do ano 1600 e imprime, anualmente, a partir do fim de 1600, o número de nascimentos, mortes e o total da população por ano (apenas a parte inteira do números, separados por vírgula). O programa encerra sua execução quanto a população total cai para menos de 10% da população original.'''
+
+def populacao(pi):
+    pa = pi
+    ano = 1600
+    pi *= 0.1
+    while pa >= pi:
+        n = pa / 100
+        mortes = pa / 100 * 6
+        pa = pa + n - mortes
+        print(f"{ano},{round(n)},{round(mortes)},{round(pa)}")
+        ano += 1
+
+populacao(int(input()))

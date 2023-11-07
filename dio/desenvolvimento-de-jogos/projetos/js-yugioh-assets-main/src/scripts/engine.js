a7a8e02678e42564691d16328158fa245996cc5e
davidplay7 <72691307+davidplay7@users.noreply.github.com>
2023-11-07 15:11:17 -0300

    inicio da engine

---
+++ dio/desenvolvimento-de-jogos/projetos/js-yugioh-assets-main/src/scripts/engine.js
@@ -0,0 +1,30 @@
+const state = {
+    score:{
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById("score_points"),
+    },
+    carsSprits:{
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    },
+    fieldCards:{
+        player: document.getElementById("player-field-card"),
+        player: document.getElementById("computer-field-card"),
+    },
+    actions:{
+        button: document.getElementById("next-duel"),
+    },
+    
+};
+const players = {
+    players
+}
+
+function init(){
+
+}
+
+init();
+

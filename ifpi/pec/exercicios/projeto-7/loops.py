03d1e0d469ecdbd8287bdd9e0c74401750c4ec77
David Sousa <72691307+davidplay7@users.noreply.github.com>
2023-11-20 18:19:35 +0000

    Finalizando as ultimas

---
+++ ifpi/pec/exercicios/projeto-7/loops.py
@@ -0,0 +1,17 @@
+alfabeto = "abcdefghijklmnopqrstuvwxyz"
+mensagem = input("Por favor, entre com a mensagem a ser criptografada: ").lower()
+mensagemCriptografada = ""
+
+chave = input("Por favor, entre a chave: ")
+
+chave = int (chave)
+
+
+for char in mensagem:
+    if char in alfabeto:
+        posicao = alfabeto.find(char)
+        novaPosicao = (posicao + chave) % 26
+        mensagemCriptografada = mensagemCriptografada + alfabeto[novaPosicao]
+    else:
+        mensagemCriptografada = mensagemCriptografada + char
+print("Sua mensagem criptografade é: ", mensagemCriptografada)

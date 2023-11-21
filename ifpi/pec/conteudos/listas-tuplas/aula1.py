b78d6ceb082844625787549541a1532b49da5aa0
davidplay7 <davidk1k3k@gmail.com>
2023-11-21 15:00:51 -0300

    fim aula

--- ifpi/pec/conteudos/listas-tuplas/aula1.py
+++ ifpi/pec/conteudos/listas-tuplas/aula1.py
@@ -13,3 +13,13 @@ print(lista2.sort())#ordena lista
 print(lista2)
 print(sorted(lista2))
 del lista2[3:5]#fatia a ser removida
+
+lista2.append("x")
+lista2.insert(0,"x")
+lista2.insert(5,"x")
+print(lista2)
+print(lista2.index("x"))
+print(lista2.index("x", 1))#elemento , apartir de
+print(lista2.index("x", 6, len(lista2)))#elemento , apartir de , ate o tamanho
+#print(lista2.index("y")) #Errado
+print("y" in lista2)#retorna True se tiver o elemento na lista

44db151ea4671504124c9af9bcc61f61cd22f25d
davidplay7 <davidk1k3k@gmail.com>
2023-11-21 14:36:24 -0300

    aula1

---
+++ ifpi/pec/conteudos/listas-tuplas/aula1.py
@@ -0,0 +1,15 @@
+#funcoes
+lista = [1,2,3,4,5]
+print(len(lista))
+lista.append(6)
+print(lista)
+del lista[5]
+print(lista)
+#
+lista2 = [7,4,2,9,5,3,8,6,1,0]
+print(max(lista2))
+print(min(lista2))
+print(lista2.sort())#ordena lista
+print(lista2)
+print(sorted(lista2))
+del lista2[3:5]#fatia a ser removida

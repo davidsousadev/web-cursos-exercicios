7acbd23d065e3281fc5d8100ce00b0480aff090f
davidplay7 <davidk1k3k@gmail.com>
2023-11-21 10:10:50 -0300

    Projeto 7 finalizado

--- ifpi/pec/exercicios/projeto-7/calculadora_do_amor.py
+++ ifpi/pec/exercicios/projeto-7/calculadora_do_amor.py
@@ -3,12 +3,13 @@ print("<3 <3 <3 <3 <3 <3 <3")
 nomes = input("Digite o nome de 2 pessoas: ").lower()
 nomes = nomes.replace(" ", "")
 placar = 0
-if nomes in "aeiou":
-    placar += 5
-if nomes in "amor":
-    placar +=10
+for nome in nomes:
+    if nome in "aeiou":
+        placar += 5
+    if nome in "amor":
+        placar += 10
 if placar < 10:
     print("Esqueça esta pessoa! Nunca vai da certo!")
-if placar>=10:
-    print(f"Sem placar de compatibilidade : {placar}")
+if placar >= 10:
+    print(f"Seu placar de compatibilidade : {placar}")
     print("Vocês terão um relacionamento muito intenso! <3")

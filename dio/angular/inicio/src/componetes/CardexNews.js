77f442e47ad1a51d150be84bac6ca8056cf4980b
davidplay7 <davidk1k3k@gmail.com>
2023-11-22 12:23:19 -0300

    finalizado

--- dio/angular/inicio/src/componetes/CardexNews.js
+++
@@ -1,9 +0,0 @@
-class CardNews extends HTMLElement{
-    constructor(){
-        super();
-        const shadow =  this.attachShadow({mode: "open"});
-        shadow.innerHTML = this.getAttribute("titulo");
-    }
-}
-
-customElements.define('card-news', CardNews);

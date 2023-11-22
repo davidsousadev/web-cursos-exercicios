f197eb027abcafc2f7dbc960e5e569f60f9e291b
davidplay7 <davidk1k3k@gmail.com>
2023-11-22 11:20:44 -0300

    Continuação Componentes

--- dio/angular/inicio/src/componetes/CardexNews.js
+++ dio/angular/inicio/src/componetes/CardexNews.js
@@ -2,7 +2,7 @@ class CardNews extends HTMLElement{
     constructor(){
         super();
         const shadow =  this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Hello Wold</h1>";
+        shadow.innerHTML = this.getAttribute("titulo");
     }
 }
 

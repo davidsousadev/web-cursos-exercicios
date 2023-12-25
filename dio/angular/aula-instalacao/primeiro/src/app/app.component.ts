2756a2945573081cdd0608ecaefe737c6ed3a78f
davidplay7 <davidk1k3k@gmail.com>
2023-12-25 18:35:26 -0300

    Add a button component and use it in the app component. (generated by blackbox.ai )

--- dio/angular/aula-instalacao/primeiro/src/app/app.component.ts
+++ dio/angular/aula-instalacao/primeiro/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {TitleComponent} from './title/title.component'
+import {BotaoComponent} from './botao/botao.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TitleComponent],
+  imports: [CommonModule, RouterOutlet, TitleComponent, BotaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

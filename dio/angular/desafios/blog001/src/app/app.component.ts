3ffbd998cfe941241a3069c2f377727ab1b11fe7
davidplay7 <davidk1k3k@gmail.com>
2024-01-06 21:13:18 -0300

    Criado menu Title

---
+++ dio/angular/desafios/blog001/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'p-n16-16-0-a-14-1-2-diretivas';
+  isAliveCard = true;
+}

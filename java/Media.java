f5fb45944c8d8aa9ff23e7a658d73995e9941f1a
davidplay7 <davidk1k3k@gmail.com>
2024-02-08 12:18:53 -0300

    Media em Java

---
+++ java/Media.java
@@ -0,0 +1,21 @@
+public class Media { 
+
+     public static void main(String[] args) {
+
+          double N1, N2, N3, N4, RES;     
+          System.out.print("Nota 1: ");
+          N1 = Double.parseDouble(System.console().readLine());
+          System.out.print("Nota 2: ");
+          N2 = Double.parseDouble(System.console().readLine());
+          System.out.print("Nota 3: ");
+          N3 = Double.parseDouble(System.console().readLine());
+          System.out.print("Nota 4: ");
+          N4 = Double.parseDouble(System.console().readLine());
+
+          long Media = Math.round(((N1 + N2 + N3 + N4) / 4.0));       
+
+          System.out.println("Media: " + Media);
+
+     }
+
+}

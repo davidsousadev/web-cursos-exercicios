d9a55921112170be0da13f39bc93d3f17e6f57b0
davidplay7 <davidk1k3k@gmail.com>
2024-02-19 11:02:39 -0300

    primeira producao angular

--- dio/angular/services-001/src/app/services/pokemon.service.ts
+++ dio/angular/services-001/src/app/services/pokemon.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {environment} from 'src/environments/environment';
+//import {environment} from 'src/environments/environment';
+//import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod'; // para produção
+
 import {PokemonData} from 'src/app/components/models/pokemonData';
 @Injectable({
   providedIn: 'root'

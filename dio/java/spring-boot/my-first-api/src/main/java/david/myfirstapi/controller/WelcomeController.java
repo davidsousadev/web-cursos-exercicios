f79b449b1aac190b2a7626e91912784d198dc6e1
davidplay7 <davidk1k3k@gmail.com>
2024-02-29 22:54:17 -0300

    inicio api spring

---
+++ dio/java/spring-boot/my-first-api/src/main/java/david/myfirstapi/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package david.myfirstapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome(){
+        return "Seja bem vindo";
+    }
+}

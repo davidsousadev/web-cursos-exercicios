3c9038e0540d1eb08c11882b2f36370cf9ed4d78
davidplay7 <davidk1k3k@gmail.com>
2024-03-01 13:00:14 -0300

    atualização primeira classe

--- ifpi/poo/primeiraClasse.py
+++ ifpi/poo/primeiraClasse.py
@@ -4,12 +4,16 @@ Dica:
 1.A classe tem que ter pelo menos um atributo mutável.
 2. Os métodos devem ter relação com algum atributo mutável.
 '''
+import time
+
+
+
 class computador:
     modelo=None
     marca=None
     cpu=None
     memoria_atual=0
-    memori_ram_max=None
+    memoria_ram_max=None
     capacidade_hd=0
     capacidade_hd_max=None
     arquivos = {}
@@ -17,6 +21,7 @@ class computador:
     estado='desligado'
     
     def ligar(self):
+        countdown_timer(6)
         self.estado = 'ligado'
     def desligar(self):
         self.estado = 'deligado'
@@ -39,8 +44,9 @@ class computador:
         if self.estado == 'ligado':
             for x in range(0,self.quantidade_arquivos):
                 print(f"Nome: {self.arquivos[x][0]} | {self.arquivos[x][1]}")
-        return f"{self.quantidade_arquivos} arquivos"
-
+            return f"{self.quantidade_arquivos} arquivos"
+        else:
+            return f"Computador Desligado!"
         
 
     def deletar_arquivo(self):
@@ -71,52 +77,81 @@ class computador:
                 return f"Apagado com sucesso!!!"
             else:
                 return f"Arquivo {nome_arquivo} não encontrado!!!" 
+        else:
+            return f"Computador Desligado!"
 
+
+
+def informacao(info):
+    if info==1:
+        return f"HD 1000 GB"
+    elif info==2:
+        return f"8GB"
+    elif info==3:
+         return f"I5"
+    elif info==4:
+        return f"Dell"
+    elif info==5:
+        return f"Inpirion 3360"
+    elif info==6:
+        return f"Ligando aguarde"
     
-         
-dell_inspiron = computador()
-dell_inspiron.modelo = "Inpirion 3360"
-dell_inspiron.marca = "Dell"
-dell_inspiron.cpu = "I5"
-dell_inspiron.memori_ram_max = "8GB"
-dell_inspiron.capacidade_hd_max = 1000
+    
 
-while True:
-    try:
-        opcao = int(input("""
+
+def countdown_timer(seconds):
+    while seconds:
+        print(informacao(seconds))
+        time.sleep(2)
+        seconds -= 1
+
+while True:       
+    ligar = int(input("Deseja ligar o computador Inpirion 3360:\n1-Sim \n2-Não\n"))
+
+    if(ligar==1):
+        dell_inspiron = computador()
+        dell_inspiron.modelo = "Inpirion 3360"
+        dell_inspiron.marca = "Dell"
+        dell_inspiron.cpu = "I5"
+        dell_inspiron.memoria_ram_max = "8GB"
+        dell_inspiron.capacidade_hd_max = 1000 
+        dell_inspiron.ligar()
+        print(f"Computador {dell_inspiron.estado}.")
+        while True:
+            try:
+                opcao = int(input("""
 =============== MENU INICIAR ===============
 Escolha uma opção:
-1 - Liga o computador
-2 - Desliga o computador
-3 - Adicionar arquivo
-4 - Status da capacidade do HD
-5 - Listar arquivos
-6 - Deletar arquivo
-0 - Sair
+1 - Desliga o computador
+2 - Adicionar arquivo
+3 - Status da capacidade do HD
+4 - Listar arquivos
+5 - Deletar arquivo
 """))
-    
-        if opcao==1:
-           dell_inspiron.ligar()
-           print(f"Computador {dell_inspiron.estado}.\n")
-        elif opcao==2:  
-            dell_inspiron.estado = 'desligado'
-            print(f"Computador {dell_inspiron.estado}.\n")
-        elif opcao==3:
-            if dell_inspiron.estado == 'ligado':
-                nome = str(input("Digite nome do arquivo: ").strip())
-                tamanho = int(input("Digite o Tamanho do arquivo em GB: "))
-                dell_inspiron.criar_arquivo(nome, tamanho)
-            else:
-                print("Computador Desligado!")       
-        elif opcao==4:
-            print(f"Capacidade disponivel do HD está em {dell_inspiron.capacidade_hd_max} GB")
-        elif opcao==5:
-            print(dell_inspiron.listar_arquivos())
-        elif opcao==6:
-            print(dell_inspiron.deletar_arquivo())    
-        elif opcao==0:
-            break
-        else:
-            print("Opção invalida!")
-    except:
-        print(f"Erro na operação!!!")
+                if opcao==1:  
+                    dell_inspiron.estado = 'desligado'
+                    print(f"Computador {dell_inspiron.estado}.\n")
+                    break
+                elif opcao==2:
+                    if dell_inspiron.estado == 'ligado':
+                        nome = str(input("Digite nome do arquivo: ").strip())
+                        tamanho = int(input("Digite o Tamanho do arquivo em GB: "))
+                        dell_inspiron.criar_arquivo(nome, tamanho)
+                    else:
+                        print("Computador Desligado!")       
+                elif opcao==3:
+                    if dell_inspiron.estado == 'ligado':
+                        print(f"Capacidade disponivel do HD está em {dell_inspiron.capacidade_hd_max} GB")
+                    else:
+                        print("Computador Desligado!")   
+                elif opcao==4:
+
+                    print(dell_inspiron.listar_arquivos())
+                elif opcao==5:
+                    print(dell_inspiron.deletar_arquivo())    
+                else:
+                    print("Opção invalida!")
+            except:
+                print(f"Erro na operação!!!")
+    else:
+        break

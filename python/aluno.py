b2e630712bbc1c982326d9a07278050d6cbedb61
davidplay7 <davidk1k3k@gmail.com>
2024-03-19 15:43:11 -0300

    add libs

---
+++ python/aluno.py
@@ -0,0 +1,4 @@
+def nota3(nota):
+    return nota*3
+def nota2(nota):
+    return nota*2
--- python/index.py
+++ python/index.py
@@ -1,34 +1,8 @@
-# Importando os módulos necessários do Kivy
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
+import david
+from aluno import nota3
+def main():
+    print(f"{david.soma()}")
+    print(f"{nota3(10)}")
 
-# Definindo a classe principal do aplicativo
-class SimpleApp(App):
-    
-    # Método para construir a interface do usuário
-    def build(self):
-        # Criando um layout de caixa vertical
-        layout = BoxLayout(orientation='vertical')
-        
-        # Criando um rótulo de texto
-        self.label = Label(text="Clique no botão para exibir uma mensagem")
-        
-        # Criando um botão
-        button = Button(text="Clique Aqui!")
-        button.bind(on_press=self.on_button_click)  # Vinculando o evento de clique do botão
-        
-        # Adicionando os widgets ao layout
-        layout.add_widget(self.label)
-        layout.add_widget(button)
-        
-        return layout
-    
-    # Método chamado quando o botão é clicado
-    def on_button_click(self, instance):
-        self.label.text = "Olá, Mundo!"
-
-# Instanciando e executando o aplicativo
-if __name__ == "__main__":
-    SimpleApp().run()
+if __name__ == '__main__':
+    main()
